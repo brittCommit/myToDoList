01e0b4dba1e4c692844d9595cc88e9ea1e7f94fc
Brittney Huttner <brittneyhuttner@gmail.com>
2020-09-22 10:32:01 -0700

    Created default data file

---
+++ my-to-do-list/src/ToDoData.js
@@ -0,0 +1,28 @@
+export default = [
+    {
+        id: 1,
+        text: "Take out recycling",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Feed Gypsy",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Go to spin",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Get a job",
+        completed: false
+    },
+    {
+        id: 5,
+        text: "Learn React",
+        completed: false
+    }
+]
+
