3516c585a89b0faf152d8d601d361ce9f7a71b27
Brittney Huttner <brittneyhuttner@gmail.com>
2020-09-22 10:42:41 -0700

    display checked box if completed is true

--- my-to-do-list/src/ToDoItem.js
+++ my-to-do-list/src/ToDoItem.js
@@ -4,7 +4,7 @@ import App from "./App";
 function ToDoItem(props) {
     return (
     <div className = "todo-list ">
-        <input type = "checkbox"/>
+        <input type = "checkbox" checked={props.item.completed}/>
         <p>{props.item.text}</p>
     </div>
     );
