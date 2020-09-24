f55e4e783ef4a8d5d52028b4eabe0b73440ff1af
Brittney Huttner <brittneyhuttner@gmail.com>
2020-09-23 19:37:34 -0700

    added conditional styling

--- my-to-do-list/src/ToDoItem.js
+++ my-to-do-list/src/ToDoItem.js
@@ -1,16 +1,21 @@
 import React from "react";
-import App from "./App";
 
 function ToDoItem(props) {
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+    }
+    
     return (
-    <div className = "todo-list ">
-        <input type = "checkbox" 
-        checked={props.item.completed}
-        onChange={() => props.handleChange(props.item.id)}
-        />
-        <p>{props.item.text}</p>
-    </div>
-    );
+        <div className="todo-item">
+            <input 
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={() => props.handleChange(props.item.id)}
+            />
+            <p style={props.item.completed ? completedStyle: null}>{props.item.text}</p>
+        </div>
+    )
 }
-
 export default ToDoItem;
